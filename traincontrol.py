1f9099d9e2f8d8e05177c8e6b3ef2bc6c97015d3
Onktom <Onk@tom>
2018-09-08 16:15:28 +0200

    Übernimm funktionierende Version vom RPI

--- traincontrol.py
+++ traincontrol.py
@@ -33,29 +33,34 @@ def main():
 
     GPIO.setup(16, GPIO.OUT)							# Weiche rechts (von hinten)
     GPIO.setup(20, GPIO.OUT)							# Weiche links(von hinten)
-    GPIO.setup(21, GPIO.OUT)							# WeichenSchaltStrom
+    GPIO.setup(12, GPIO.OUT)							# WeichenSchaltStrom
 
     GPIO.setup(23, GPIO.OUT)							# Stopp-Stelle 1 hinteres Gleis
     GPIO.setup(24, GPIO.OUT)							# Stopp-Stelle 2 Vordergrund
     GPIO.setup(25, GPIO.OUT)							# Stopp-Stelle 3 vorderes Gleis
-
+    GPIO.setup(18, GPIO.OUT)							# Signal
+    
     print ('Start des Programms')
     print ('S   Stopp-Stellen werden aktiviert')
     GPIO.output(23, GPIO.LOW)							# Stopp 1 - hinteres Gleis
     GPIO.output(24, GPIO.LOW)							# Stopp 2 - Vordergrund
     GPIO.output(25, GPIO.LOW)							# Stopp 3 - vorderes Gleis
+    GPIO.output(18, GPIO.HIGH)							# Signal: Halt
 
     print ("S   Hauptschalter fuer Weichen aus")         
-    GPIO.output(21, GPIO.HIGH)											# Hauptschalter fuer Weichen aus
+    GPIO.output(12, GPIO.HIGH)							# Hauptschalter fuer Weichen aus
+    
+    #EsFaehrt == 0
     
     def Weichen_schalten():
         print
         print ('W   Weichen werden geschaltet')
         print
-        GPIO.output(21, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
         time.sleep(0.1)
-        GPIO.output(21, GPIO.HIGH)										# Hauptschalter fuer Weichen aus
-    
+        GPIO.output(12, GPIO.HIGH)										# Hauptschalter fuer Weichen aus
+        print "Weichen sind geschaltet"
+         
     def Zugauswahl(x):
         print
         print ('A   Gleis bestimmen')
@@ -72,22 +77,22 @@ def main():
 
         if Gleis[0] == "1":
             print ("A   Schaltung Gleis 1")
-            GPIO.output(16, GPIO.LOW)  # Weiche 1 aussen
-            GPIO.output(20, GPIO.LOW)  # Weiche 3 aussen
+            GPIO.output(16, GPIO.HIGH)  								# Weiche 1 aussen
+            GPIO.output(20, GPIO.HIGH)  								# Weiche 3 aussen
             print ("A   Weichen_schalten")
             Weichen_schalten()
-            GPIO.output(33, GPIO.HIGH)  # Stopp 1 aus, Fahrt
-            time.sleep(15)
-            EsFaehrt == 1
+            GPIO.output(23, GPIO.HIGH)  								# Stopp 1 aus, Fahrt
+            #time.sleep(15)
+            #EsFaehrt == 1
 
         else:
             print ("A   Schaltung Gleis 3")
-            GPIO.output(16, GPIO.HIGH)  # Weiche 1 innen
-            GPIO.output(20, GPIO.HIGH)  # Weiche 3 innen
+            GPIO.output(16, GPIO.LOW)  								# Weiche 1 innen
+            GPIO.output(20, GPIO.LOW)  								# Weiche 3 innen
             print ("A   Weichen_schalten")
             Weichen_schalten()
-            GPIO.output(33, GPIO.HIGH)  # Stopp 3 aus, Fahrt
-            EsFaehrt == 3
+            GPIO.output(25, GPIO.HIGH)  								# Stopp 3 aus, Fahrt
+            #EsFaehrt == 3
 
     print
     print "Zugauswahl zum Programmstart aufrufen"         
@@ -100,22 +105,29 @@ def main():
     # Main loop. Das Programm verweilt nun bis zu seinem Ende in dieser loop.
     while True:
 
-        print "test Input1"
+        #print "test Input1"
         if GPIO.input(6) == 0:
-          print ('Schalter 1 neu ... Gleis 1, Ausfahrt')
+          print ('Schalter 1')
           
-        print "test Input2"
+        #print "test Input2"
         if GPIO.input(13) == 0:
-          print ('Schalter 4 neu ... Gleis 2 Einfahrt')
+          print ('Schalter 2')
+          time.sleep(8)
+          GPIO.output (18, GPIO.LOW)									#Signal: Fahrt
+          time.sleep(3)
+          GPIO.output (24, GPIO.HIGH)									#Stopp2: Fahrt
+          time.sleep(8)
+          GPIO.output (24, GPIO.LOW)									#Stopp2: Halt
+          GPIO.output (18, GPIO.HIGH)									#Signal: Halt
           
-        print "test Input3"
+        #print "test Input3"
         if GPIO.input(19) == 0:
-          print ('Schalter 2 neu ... Gleis 2 Ausfahrt')
+          print ('Schalter 3')
           
-        print "test Input4"
-        if GPIO.input(26) == 0:
-          print ('Ausschalter')
-          os.system("sudo shutdown -h now -P 0")						# System herunterfahren
+        #print "test Input4"
+        #if GPIO.input(26) == 0:
+          #print ('Ausschalter')
+          #os.system("sudo shutdown -h now -P 0")						# System herunterfahren
           
 
 # If run this script directly from the shell, start it:
